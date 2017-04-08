014a1c0408c290df01b5309bb72fd31f5c9a4fea
Gabriel Pereira <gsilvap1994@gmail.com>
2017-04-08 15:04:27 -0300

    subindo

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var MongoClient = require('mongodb').MongoClient
+
+var app = express()
+app.use(bodyParser.json())
+
+MongoClient.connect('mongodb://demouser:demo123@ds149278.mlab.com:49278/demo-db-gama', function (err, db) {
+    if (err) {
+        throw err;
+    }
+
+    app.listen(3000, function () {
+        console.log('Example app listening on port 3000!');
+    });
+
+    require('./routes/user.route')(app, db);
+
+    console.log('banco de dados conectado')
+});
